373eb764f82aad389e43f6cf1613f115cd373140
yOoMarvin <marvin.messenzehl@gmail.com>
2017-04-18 18:57:20 +0200

    Adding colors and a first label

--- my-100daysofcode-app/StartViewController.swift
+++ my-100daysofcode-app/StartViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+//    var titleLabel: UILabel!
+    
+    // Color properties
+    let darkBlueColor = UIColor(red:0.00, green:0.26, blue:0.35, alpha:1.0)
+    let mintGreenColor = UIColor(red:0.12, green:0.54, blue:0.44, alpha:1.0)
+    let limeGreenColor = UIColor(red:0.75, green:0.86, blue:0.22, alpha:1.0)
+    let lemonYellowColor = UIColor(red:1.00, green:0.88, blue:0.10, alpha:1.0)
+    let orangeColor = UIColor(red:0.99, green:0.45, blue:0.00, alpha:1.0)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //test setup
-        view.backgroundColor = UIColor.yellow
-        
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 150, width: 50, height: 50)
-        label.text = "Hello"
-        self.view.addSubview(label)
+//        view.backgroundColor = UIColor.white
+//        
+//        titleLabel = UILabel()
+//        titleLabel.frame = CGRect(x: 8, y: 24, width: self.view.frame.size.width , height: 60)
+//        titleLabel.text = "my 100 days of code"
+//        titleLabel.font = UIFont(name: "AvenirNext-Regular", size: 32)
+//        titleLabel.textColor = UIColor.black
+//        self.view.addSubview(titleLabel)
     }
 
     
