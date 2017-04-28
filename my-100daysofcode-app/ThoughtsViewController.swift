f8f40df95f3c14046c9c51e5340e05bceb7f6516
yOoMarvin <marvin.messenzehl@gmail.com>
2017-04-28 19:50:06 +0200

    Added view elements and controller class

---
+++ my-100daysofcode-app/ThoughtsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ThoughtsViewController.swift
+//  my-100daysofcode-app
+//
+//  Created by Marvin Messenzehl on 28.04.17.
+//  Copyright © 2017 Marvin Messenzehl. All rights reserved.
+//
+
+import UIKit
+
+class ThoughtsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
